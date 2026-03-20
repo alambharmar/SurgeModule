bb707b1b76018d232c3077685281551be1dd58e4
alambharmar <alam.bharmal@gmail.com>
2026-03-20 16:51:16 +0530

    Update license information in Alcove.js

--- Alcove/Alcove.js
+++ Alcove/Alcove.js
@@ -4,14 +4,14 @@ const license$ = {
     key: "88888888-8888-8888-8888-888888888888",
     active: true,
     beta: false,
-    name: "InjectX-Team",
-    email: "injectxteam@gmail.com",
-    limit: 999,
+    name: "Alam",
+    email: "alam@gmail.com",
+    limit: 3,
     usage: 1,
     instance: {
         id: "fca4add6-fc53-51da-826e-1b67f43f7f3b",
-        name: "InjectX",
-        model: "team@injectx.com"
+        name: "alam",
+        model: "alam.com"
     }
 };
 
